6282ec998dcfd5037c711e6f09f1eee4394902e3
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-01-19 15:37:01 +0530

    Build of page completed(HTML and major css)

---
+++ components/prodCatalogbottom.js
@@ -0,0 +1,246 @@
+function mobileDescriptions(){
+    return `<div id="bigDescriptionDiv">
+    <div id="bdLeft">
+        <h3><b> Buy All Smartphones Online in India</b></h3>
+        
+        <p>Mobile phones have transformed from being a device to make calls or send texts, to an entertainment and information gadget. Today, it’s practically impossible to spend a moment without our smartphones by our side- whether it’s just for sending an email, storing important files, doing a video call with our loved ones, or streaming the latest web series while commuting. Mobile phones come with a wide range of features these days- like fascinating photo filters, AI-powered cameras, blazing fast processing speeds, etc. At Reliance Digital, you can compare and find your choice of mobile phone online at the best prices.</p>
+        
+        <h3>Choosing A Smartphone As Per Operating System</h3>
+        
+        <p>With hundreds of options available, the online mobile shopping experience can be quite overwhelming if you don’t know what you want. Begin with choosing the type of operating system you’d be comfortable using. There are only two smartphone operating systems worth considering: Android and iOS.</p>
+        
+        <p>Here’s a comparison between Android phones and iOS phones that can help you choose better:</p>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Features</th>
+                    <th>Android</th>
+                    <th>iOS</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>Latest Stable Release</th>
+                    <td>Android 10</td>
+                    <td>iOS 13.3.1 and iPadOS 13.3.1</td>
+                </tr>
+                <tr>
+                    <th>Customizability</th>
+                    <td>A lot. Can change almost anything</td>
+                    <td>Limited unless jailbroken</td>
+                </tr>
+                <tr>
+                    <th>Internet Browsing</th>
+                    <td>Google Chrome (other browsers are available)</td>
+                    <td>Safari (Other browsers are availbale but not as defualt)</td>
+                </tr>
+                <tr>
+                    <th>Web Mapping Service</th>
+                    <td>Google Maps</td>
+                    <td>Apple Maps, default as of iOS 6 (Google Maps also available via a separate app download)</td>
+                </tr>
+                <tr>
+                    <th>Calls and Messaging</th>
+                    <td>Google Messages. 3rd party apps like Facebook Messenger, WhatsApp, Google Due, Discord, and Skype all work on Android and iOS.</td>
+                    <td>iMessage, Face Time (with other Apple devices only), and the rest of teh apps as in Android</td>
+                </tr>
+                <tr>
+                    <th>App Store</th>
+                    <td>Google Play Store - 2,000,000+ apps. Other app stores like Amazon and Aptoide also distribute Android apps.</td>
+                    <td>Apple App Store - 1,000,000+ apps. Apps containing viruses are very rare or nonexistent</td>
+                </tr>
+                <tr>
+                    <th>Photos & Videos Backup</th>
+                    <td>Apps are available for an automatic backup of photos and videos. Google photos allows unlimited backup of photos at a compressed quality</td>
+                    <td>Up to 5 GB of photos and videos can be automatically backed up with iCloud, with more paod iCloud storage available via subscription.</td>
+                </tr>
+                <tr>
+                    <th>Security</th>
+                    <td>Monthly security updates</td>
+                    <td>Occasional security updates. Security threats are rare.</td>
+                </tr>
+            </tbody>
+        </table>
+        <h3>Buy Mobile Phones As Per Price Band</h3>
+        <h3>Features of mobile phones below 5000</h3>
+        <ul>
+            <li><span class="liMark">-</span>  Dual SIM</li>
+            <li><span class="liMark">-</span> 1-3GB RAM</li>
+            <li><span class="liMark">-</span> 16-32 GB inbuilt memory</li>
+        </ul>
+        <h3>Features of mobile phones below 10000</h3>
+        <ul>
+            <li><span class="liMark">-</span>Dual camera</li>
+            <li><span class="liMark">-</span>2 to 4GB RAM</li>
+            <li><span class="liMark">-</span>5.5 inch + screen size</li>
+            <li><span class="liMark">-</span>3000 mAH + battery</li>
+        </ul>
+        <h3>Features of moble phones below 20000</h3>
+        <ul>
+            <li><span class="liMark">-</span>Dual camera</li>
+            <li><span class="liMark">-</span>2 to 4GB RAM</li>
+            <li><span class="liMark">-</span>5.5 inch + screen size</li>
+            <li><span class="liMark">-</span>3000 mAH + battery</li>
+        </ul>
+        <h3>Features of mobile phones below 30000</h3>
+        <ul>
+            <li><span class="liMark">-</span>8 GB RAM</li>
+            <li><span class="liMark">-</span>6.4 inches camera</li>
+            <li><span class="liMark">-</span>4000 mAh Battery</li>
+            <li><span class="liMark">-</span>Octa-Core, Snapdragon processor</li>
+        </ul>
+        <h3>Features of mobile phones below 40000</h3>
+        <ul>
+            <li><span class="liMark">-</span>Premium design</li>
+            <li><span class="liMark">-</span>Up to 64 MP camera sensor</li>
+            <li><span class="liMark">-</span>Super AMOLED screen</li>
+            <li><span class="liMark">-</span>Flagshp grade Snapdragn processor</li>
+        </ul>
+        <h3>Features of mobile phones below 50000</h3>
+        <ul>
+            <li><span class="liMark">-</span>Triple camera</li>
+            <li><span class="liMark">-</span>Top-notch camera and video-shooting modes</li>
+            <li><span class="liMark">-</span>Full-screen HD display</li>
+            <li><span class="liMark">-</span>Stereo speakers</li>
+        </ul>
+        <h3>Top Mobile Phone Brands</h3>
+        <ol>
+            <li>
+                <b>Samsung Mobile Phones:</b>
+                <ul>
+                    <li><p>Samsung is India’s most attractive and trusted brand of mobiles. It has one of the largest lower-priced handsets markets in India. They usually have bigger screens (Around 6-6.4 inches) and have a simple, user-friendly design. They are renowned for their Qualcomm Snapdragon processors and great storage capacity.
+
+                    </p></li>
+                </ul>
+            </li>
+            <li>
+                <b>Oneplus Mobile Phone:</b>
+                <ul>
+                    <li><p>Oneplus mobile phones are considered as one of the best mid-premium android smartphones in the world. Their mobile is designed to be the perfect companion for all your daily activities in this modern mobile technology.
+
+                    </p></li>
+                </ul>
+            </li>
+            <li>
+                <b>Vivo Mobile Phone:</b>
+                <ul>
+                    <li><p>Vivo is known for its low-cost and mid-range segment Android phones, the highlight of these phones being camera quality. Their mobile handset is sleeked in design having brilliant camera specifications with impeccable performance and long-lasting battery life. Vivo phones also come with a lot of sensors like proximity sensors, accelerometers, ambient light sensors, etc.
+                    </p></li>
+                </ul>
+            </li>
+            <li>
+                <b>Oppo Mobile Phone:</b>
+                <ul>
+                    <li><p>Oppo mobiles are best known for their impeccable cameras and stylish aesthetics. Oppo phones are selfie experts. The screen is ultra-bright and crisp, and the battery charges really fast.
+                    </p></li>
+                </ul>
+            </li>
+            <li>
+                <b>Apple Mobile Phone:</b>
+                <ul>
+                    <li><p>iPhones are the world's most powerful personal devices. Reception ability, design, software, loudness, and vocal clarity are also superior to most other phones. The camera and image quality of iPhones are unparalleled, they come with a wide range of picture/video modes- the Bokeh mode being the most popular these days. Gaming, photography, videos or storage, iPhone tops in everything.
+
+                    </p></li>
+                </ul>
+            </li>
+            <li>
+                <b> Xiaomi Mobile Phone:</b>
+                <ul>
+                    <li><p>They are impressive phones focusing mainly on strong battery, multitasking, unique user interface (MIUI), and a strong body build. A must-buy if you want an all-rounder smartphone. They have a strong operating system that allows a better customer experience of mobile phones with reliable quality.
+                    </p></li>
+                </ul>
+            </li>
+        </ol>
+        <h3>Compare and Choose The Best Mobile Phones Online</h3>
+        <p>At <a href="#">Reliance Digital</a>, you can explore smartphones that are pocket-friendly or high on features, all under one roof. Following are some specifications to consider while buying a mobile phone online:</p>
+        <ol>
+            <li>Camera quality</li>
+            <li>Processor</li>
+            <li>Build quality</li>
+            <li>Display</li>
+            <li>Battery</li>
+            <li>User interface</li>
+            <li>Storage</li>
+            <li>Extra features</li>
+            <li>Audio/speakers</li>
+        </ol>
+        <p>Buying a smartphone online is now so easy and simple</p>
+        <hr>
+        <h2>POPULAR SEARCHES:</h2>
+        <p class="red">Mobile Phones Under 5000 | Mobile Phones Under 7000 | Mobile Phones Under 10000 | Mobile Phones Under 15000 | Mobile Phones Under 20000</p>
+    </div>
+    <div id="bdRight">
+        <h2>Smartphones Price List</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>S.no</th>
+                    <th>Products-Smartphones</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td class="linkify">Apple iPhone 13 128Gb, Midnight (Black)</td>
+                    <td>₹79,900.00</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td class="linkify">OnePlus Nord CE 5G 256 GB, 12 GB RAM. Silver Ray, Smartphone</td>
+                    <td>₹27,999.00</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td class="linkify">Samsung Galaxy A52s 5G 128 GB, 8 GB RAM, Black, Smartphone</td>
+                    <td>₹37,499.00</td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td class="linkify">Redmi 9A 32 GB, 2 GB RAM, Nature Green, Smartphone</td>
+                    <td>₹7,299.00</td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td class="linkify">Apple iPhone 13 Mini 128 GB, Starlight</td>
+                    <td>₹69,900.00</td>
+                </tr>
+                <tr>
+                    <td>6</td>
+                    <td class="linkify">VIVO Y51A, 128GB, 6GB RAM, Titanium Sapphire, Smartphone</td>
+                    <td>₹16,990.00</td>
+                </tr>
+                <tr>
+                    <td>7</td>
+                    <td class="linkify">Oppo Reno 6 Pro 5G 256GB, 12 GB RAM, Stellar Black Smartphone</td>
+                    <td>₹39,990.00</td>
+                </tr>
+                <tr>
+                    <td>8</td>
+                    <td class="linkify">Samsung Galaxy A03s 64 GB, 4 GB RAM, Black, Smartphone</td>
+                    <td>₹11,999.00</td>
+                </tr>
+                <tr>
+                    <td>9</td>
+                    <td class="linkify">Nokia C20 Plus 32 GB, 2 GB RAM, Ocean Blue Smartphone</td>
+                    <td>₹7,999.00</td>
+                </tr>
+                <tr>
+                    <td>10</td>
+                    <td class="linkify">realme GT Series GT-Master 5G 256GB, 8 GB RAM, Voyager Grey, Smartphone</td>
+                    <td>₹37,499.00</td>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                        Smartphones Price List updated on 18-01-2022
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>`
+}
+
+
+export default mobileDescriptions
