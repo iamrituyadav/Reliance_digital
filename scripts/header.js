4a99f5cdc9894e3c11a153497c5877fc3565b8f9
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-01-21 14:22:50 +0530

    made productcatalogfromnav.html completely responsive

--- scripts/header.js
+++ scripts/header.js
@@ -34,3 +34,23 @@ document.querySelector("#popupdiv").addEventListener("mouseleave",()=>{
         tg[i].style.display = "none";
     }
 })
+
+
+//Search functionality
+/*
+document.querySelector("***search button id here***").addEventListener("click",()=>{
+    let terms = ["phone","smartphone","mobile","vivo","oppo","iphone","samsung","realme","redmi"]
+    let searched_term = document.querySelector("***search bar id here ***").value;
+    searched_term = searched_term.toLower()
+    let term_found = false;
+    for(let i =0; i<terms.length;i++){
+        if(searched_term===terms[i]){
+            term_found = true;
+            window.location.href = "productFromSearch.html"
+        }
+    }
+    if(term_found === false){
+        alert("Invalid Search! Only Mobile Products Available")
+    }
+})
+*/
